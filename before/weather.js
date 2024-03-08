09a5e2dc90acc5d076a96324ef4fa9f091d7d9ae
Minki Baek <alsrl6678@naver.com>
2024-03-08 15:37:36 +0900

    디자인수정

--- before/weather.js
+++ before/weather.js
@@ -16,7 +16,6 @@ function callWeather() {
     .then((data) => {
       $weather_icon.src =
         "../before/assets/svg/" + data.weather[0].icon + ".svg";
-      console.log("before/assets/svg" + data.weather[0].icon + ".svg");
       // 켈빈온도 -> 섭씨온도 °C = K - 273.15
       $weather_temp.textContent = Math.round(data.main.temp - 273.15) + "°C";
     })
@@ -26,15 +25,43 @@ function callWeather() {
 }
 
 // 3초에 한번 날씨 불러오는거 갱신
-setInterval(callWeather, 1000);
+setInterval(callWeather, 3000);
 
-// 2. 카드 뒤집는 버튼 기능
+// 2. 음식정보를 보여주는 food card
+
+// 버튼
 const $recomandbtn = document.querySelector(".recomandbtn");
-
+// 카드를 담고있는 boord
 const $board = document.querySelector(".board");
 
-function showFood() {
+// 음식 사진
+const $food_image = document.querySelector("#food_image");
+
+// 음식 이름
+const $food_name = document.querySelector("#food_name");
+
+// 음식 설명
+const $food_description = document.querySelector("#food_description");
+
+// 카드 뒤집으면서 추천 음식 보여주기
+function cardflip() {
   console.log("button click");
 
   $board.classList.toggle("flip");
+
+  showfood();
+}
+
+function showfood() {
+  fetch("../before/food.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      $food_image.src = data.food[0].image;
+      $food_name.textContent = data.food[0].name;
+      $food_description.textContent = data.food[0].description;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 }
